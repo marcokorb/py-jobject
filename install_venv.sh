01e0b0e5201b59b6092a102af3243cadc3e929c3
Fernando Cicconeto <fcicconeto@gmail.com>
2019-03-06 14:22:52 -0300

    Package definition

---
+++ install_venv.sh
@@ -0,0 +1,4 @@
+python3 -m virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+pip install -r requirements_dev.txt
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+Package configuration.
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    README = readme_file.read()
+
+with open('requirements.txt', 'r') as requirements_file:
+    REQUIREMENTS = requirements_file.readlines()
+
+setup(
+    name='py-jobject',
+    version='0.1.0',
+    author='Fernando Cicconeto',
+    author_email='fcicconeto@gmail.com',
+    description= \
+        'A package to easily convert between Python objects and JSON-compatible dictionaries.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fcicc/py-jobject',
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ]
+)
