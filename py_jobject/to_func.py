2b42309df0a329e1eb3b3fc05bb3e61db2027a2d
Thais Vergani <thais.vergani@cakeerp.com>
2020-03-03 14:36:59 -0300

    Added compatibility with Union type annotation

--- py_jobject/to_func.py
+++ py_jobject/to_func.py
@@ -3,10 +3,13 @@ Functions to convert from objects to dictionaries.
 """
 
 from .definitions import get_type_definition
+from warnings import warn
+from typing import Union
 
 __all__ = [
     'to_dict'
 ]
+
 
 def to_dict(source, type_=None):
     """
@@ -19,6 +22,16 @@ def to_dict(source, type_=None):
     definition = get_type_definition(type_)
 
     if source is not None:
-        return definition.to_dict(source)
+        if hasattr(type_, '__origin__') and type_.__origin__ is Union:
+            for d in definition:
+                try:
+                    return definition.to_dict(source)
+                except Exception as e:
+                    warn("Error calling to_dict from {content} to dict".format(
+                        content=source,
+                        cls=str(d.type_)
+                    ))
+        else:
+            return definition.to_dict(source)
 
     return None
